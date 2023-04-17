9da1b561a32e41cd10f4c802e74cb8ded88e408a
IvanS4L4S <66227125+IvanS4L4S@users.noreply.github.com>
2023-04-17 17:47:01 -0300

    agragación de imagenes

--- Calculadora/Form1.cs
+++ Calculadora/Form1.cs
@@ -11,11 +11,12 @@ namespace Calculadora
 {
     public partial class Form1 : Form
     {
-        string input = string.Empty;
-        string operacion1 = string.Empty;
-        string operacion2 = string.Empty;
-        char operation;
-        double result = 0.0;
+        string ingreso = string.Empty;   //cadena que almacena la entrada del usuario
+        string operacion1 = string.Empty;  // cadena que almacena el primer operando
+        string operacion2 = string.Empty;   //cadena que almacena el segundo operando
+        char operacion;                //char para la operación
+        double result = 0.0;  //resultado calculado
+        string mensaje = "Resultado:";
         public Form1()
         {
             InitializeComponent();
@@ -29,80 +30,115 @@ namespace Calculadora
         private void button1_Click(object sender, EventArgs e)
         {
             this.textBox1.Text = "";
-            input += "1";
-            this.textBox1.Text += input;
+            ingreso += "1";
+            this.textBox1.Text += ingreso;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             this.textBox1.Text = "";
-            input += "2";
-            this.textBox1.Text += input;
+            ingreso += "2";
+            this.textBox1.Text += ingreso;
 
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             this.textBox1.Text = "";
-            input += "3";
-            this.textBox1.Text += input;
+            ingreso += "3";
+            this.textBox1.Text += ingreso;
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             this.textBox1.Text = "";
-            input += "4";
-            this.textBox1.Text += input;
+            ingreso += "4";
+            this.textBox1.Text += ingreso;
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
             this.textBox1.Text = "";
-            input += "5";
-            this.textBox1.Text += input;
+            ingreso += "5";
+            this.textBox1.Text += ingreso;
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
             this.textBox1.Text = "";
-            input += "6";
-            this.textBox1.Text += input;
+            ingreso += "6";
+            this.textBox1.Text += ingreso;
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
             this.textBox1.Text = "";
-            input += "7";
-            this.textBox1.Text += input;
+            ingreso += "7";
+            this.textBox1.Text += ingreso;
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
             this.textBox1.Text = "";
-            input += "8";
-            this.textBox1.Text += input;
+            ingreso += "8";
+            this.textBox1.Text += ingreso;
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
             this.textBox1.Text = "";
-            input += "9";
-            this.textBox1.Text += input;
+            ingreso += "9";
+            this.textBox1.Text += ingreso;
         }
 
         private void buttonSuma_Click(object sender, EventArgs e)
         {
-            operacion1 = input;
-            operation = '+';
-            input = string.Empty;
+            operacion1 = ingreso;
+            operacion = '+';
+            ingreso = string.Empty;
         }
 
         private void buttonResta_Click(object sender, EventArgs e)
         {
-            operacion1 = input;
-            operation = '-';
-            input = string.Empty;
+            operacion1 = ingreso;
+            operacion = '-';
+            ingreso = string.Empty;
         }
+        private void buttonPotencia_Click(object sender, EventArgs e)
+        {
+            operacion1 = ingreso;
+            operacion = '^';
+            ingreso = string.Empty;
+        }
+
+        private void buttonMultiplicacion_Click(object sender, EventArgs e)
+        {
+            operacion1 = ingreso;
+            operacion = '*';
+            ingreso = string.Empty;
+        }
+
+        private void buttonFactorial_Click(object sender, EventArgs e)
+        {
+            operacion1 = ingreso;
+            operacion = '!';
+            ingreso = string.Empty;
+        }
+
+        private void buttonDivicion_Click(object sender, EventArgs e)
+        {
+            operacion1 = ingreso;
+            operacion = '/';
+            ingreso = string.Empty;
+        }
+
+        private void buttonPotencia_Click_1(object sender, EventArgs e)
+        {
+            operacion1 = ingreso;
+            operacion = 'p';
+            ingreso = string.Empty;
+        }
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -116,41 +152,64 @@ namespace Calculadora
 
         private void equals_Click(object sender, EventArgs e)
         {
-            operacion2 = input;
+            operacion2 = ingreso;
             double num1, num2;
             double.TryParse (operacion1, out num1);
             double.TryParse (operacion2, out num2);
 
-            if (operation == '+')
+            if (operacion == '+')
             {
+           
             result = num1 + num2;
-            textBox1.Text = result. ToString();
+            textBox1.Text = mensaje+result. ToString();
             }
-            else if (operation == '-')
+            else if (operacion == '-')
             {
             result = num1 - num2;
             textBox1.Text = result. ToString();
             }
-            else if (operation == '*')
+            else if (operacion == '*')
             {
             result = num1 * num2;
             textBox1.Text = result. ToString();
             }
-            else if (operation == '/')
+            else if (operacion == '/')
             {
-            if (num2 != 0)
-            {
-            result = num1 / num2;
-            textBox1.Text = result.ToString();
-            }
-            else
-            {
-            textBox1.Text = "DIV/Zero!";
-            }
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                    textBox1.Text = result.ToString();
+                }
+                else
+                {
+                    textBox1.Text = "DIV/Zero!";
+                }
 
             }
-                    }
+            if (operacion == '!')
+            {
+                double acumulador=0;
+                double num=0;
+                while(num == num1){
+                    num1 = num1 - 1;
+                    acumulador=acumulador*num;
+                    num++;
+                }
+                textBox1.Text = result.ToString();
+            }
+            if (operacion == '^')
+            {
+                result = Math.Pow(num1,2);
+                textBox1.Text = result.ToString();
+            }
+            if (operacion == 'p')
+            {
+                result = Math.Pow(num1, num2);
+                textBox1.Text = result.ToString();
+            }
+         }
+
 
        
-                }
+      }
 }
